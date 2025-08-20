7ae4935080c497448df6cd44ef1bae71ace5d2dd
Sirref <Sirref@ymail.com>
2025-08-20 17:38:54 -0400

    update roles

--- src/types/role.ts
+++ src/types/role.ts
@@ -1,14 +1,30 @@
-export type Role = 'Healer' | 'AOE Healer' | 'VG/IG' | 'Melee' | 'Flail' | 'Fire' | 'Dex' | 'Tank' | 'Disruptor' | 'Shot Caller' | 'Other' | 'Many' | '';
+// export type Role = 'Healer' | 'AOE Healer' | 'VG/IG' | 'Melee' | 'Flail' | 'Fire' | 'Dex' | 'Tank' | 'Disruptor' | 'Shot Caller' | 'Other' | 'Many' | '';
+// export const kRoles: Role[] = [
+//     'Healer',
+//     'AOE Healer',
+//     'VG/IG',
+//     'Melee',
+//     'Flail',
+//     'Fire',
+//     'Dex',
+//     'Tank',
+//     'Disruptor',
+//     'Shot Caller',
+//     'Other',
+// ]
+export type Role = "Healer MB" | "Healer AOE" | "Bruiser" | "Tank" | "Shotcaller" | "Flail" | "QDPS" | "VG IG" | "Dex" | "Healer KS" | "Disruptor" | "Firestaff" | "Bow" | '' | 'Many';
 export const kRoles: Role[] = [
-    'Healer',
-    'AOE Healer',
-    'VG/IG',
-    'Melee',
-    'Flail',
-    'Fire',
-    'Dex',
-    'Tank',
-    'Disruptor',
-    'Shot Caller',
-    'Other',
+    "Healer MB",
+    "Healer AOE",
+    "Bruiser",
+    "Tank",
+    "Shotcaller",
+    "Flail",
+    "QDPS",
+    "VG IG",
+    "Dex",
+    "Healer KS",
+    "Disruptor",
+    "Firestaff",
+    "Bow",
 ]
