fffc81e5bb4459ee19b48ef4e2e87f1f8c2fef92
Sirref <Sirref@ymail.com>
2025-08-21 12:39:34 -0400

    hide captures if there is no captures available

--- src/components/atom/capturetimes.tsx
+++ src/components/atom/capturetimes.tsx
@@ -7,19 +7,24 @@ interface CaptureTimesProps {
     captures: CaptureTimes;
 }
 export function CaptureTimes({ captures }: CaptureTimesProps): JSX.Element {
-    const pa = captures.pointA ? formatSeconds(captures.pointA) : "-";
-    const pb = captures.pointB ? formatSeconds(captures.pointB) : "-";
-    const pc = captures.pointC ? formatSeconds(captures.pointC) : "-";
-    const pf = captures.fort ? formatSeconds(captures.fort) : "-";
-    return (
-        <div className="flex flex-col text-white">
-            <span className="text-sm text-center">Captures</span>
-            <div className="grid grid-cols-4 place-items-center w-full text-white text-sm">
-                <StatWithIcon icon={<div>A</div>} value={pa} />
-                <StatWithIcon icon={<div>B</div>} value={pb} />
-                <StatWithIcon icon={<div>C</div>} value={pc} />
-                <StatWithIcon icon={<div>Fort</div>} value={pf} />
+
+    if (captures.pointA || captures.pointB || captures.pointC || captures.fort) {
+        const pa = captures.pointA ? formatSeconds(captures.pointA) : "-";
+        const pb = captures.pointB ? formatSeconds(captures.pointB) : "-";
+        const pc = captures.pointC ? formatSeconds(captures.pointC) : "-";
+        const pf = captures.fort ? formatSeconds(captures.fort) : "-";
+        return (
+            <div className="flex flex-col text-white">
+                <span className="text-sm text-center">Captures</span>
+                <div className="grid grid-cols-4 place-items-center w-full text-white text-sm">
+                    <StatWithIcon icon={<div>A</div>} value={pa} />
+                    <StatWithIcon icon={<div>B</div>} value={pb} />
+                    <StatWithIcon icon={<div>C</div>} value={pc} />
+                    <StatWithIcon icon={<div>Fort</div>} value={pf} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (<></>);
+    }
 }
